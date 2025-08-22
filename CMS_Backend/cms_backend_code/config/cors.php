83b1cbc86a347392573736c43e3c525d0c83c6eb
Mohamad Moumneh <mmoumneh14@gmail.com>
2025-08-22 11:33:56 +0300

    Nginx Configuration 1 Web Server

--- CMS_Backend/cms_backend_code/config/cors.php
+++ CMS_Backend/cms_backend_code/config/cors.php
@@ -2,20 +2,17 @@
 
 return [
 
-    'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'broadcasting/auth'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://127.0.0.1:5173',
-        'http://localhost:5173',
-    ],
+    'allowed_origins' => [],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['#^https?://(localhost|127\\.0\\.0\\.1)(:\\d+)?$#'],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
